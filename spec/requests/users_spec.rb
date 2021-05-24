ef23460c6aaac81cfc5cc7ff29481e7dda8a25a0
JXDX <josh@junglescout.com>
2021-05-23 22:43:28 -0700

    add GET /users endpoint

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Users API', type: :request do
-  let(:user) { build(:user) }
+  let(:user) { create(:user) }
+  let!(:users) { create_list(:user, 9) }
   let(:headers) { valid_headers.except('Authorization') }
   let(:valid_attributes) do
     attributes_for(:user, password_confirmation: user.password)
+  end
+  # authorize request
+  let(:headers) { valid_headers }
+
+  # Test suite for GET /users
+  describe 'GET /users' do
+    # make HTTP get request before each example
+    before { get '/users', params: {}, headers: headers }
+
+    it 'returns users' do
+      # Note `json` is a custom helper to parse JSON responses
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
   end
 
   # User signup test suite
